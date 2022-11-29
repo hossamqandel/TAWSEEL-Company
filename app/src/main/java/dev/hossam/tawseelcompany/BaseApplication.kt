bba32a3253a8032d9ce3616446cc59c947337d4f
Hossam <hossamegyqandel@gmail.com>
2022-11-29 22:02:18 +0200

    initial commit

---
+++ app/src/main/java/dev/hossam/tawseelcompany/BaseApplication.kt
@@ -0,0 +1,35 @@
+package dev.hossam.tawseelcompany
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import com.akexorcist.localizationactivity.ui.LocalizationApplication
+import dagger.hilt.android.HiltAndroidApp
+import java.util.*
+
+@HiltAndroidApp
+class BaseApplication : LocalizationApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    override fun getDefaultLanguage(context: Context): Locale {
+        return Locale.ENGLISH
+    }
+
+    private fun createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val notificationChannel = NotificationChannel(
+                "foreground",
+                "dev.hossam.tawseelcompany",
+                NotificationManager.IMPORTANCE_DEFAULT)
+
+            val manager = getSystemService(NotificationManager::class.java)
+            manager.createNotificationChannel(notificationChannel)
+
+        }
+    }
+}
---
+++ app/src/main/java/dev/hossam/tawseelcompany/feature_main/presentation/MainActivity.kt
@@ -0,0 +1,14 @@
+package dev.hossam.tawseelcompany.feature_main.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import dev.hossam.tawseelcompany.R
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
