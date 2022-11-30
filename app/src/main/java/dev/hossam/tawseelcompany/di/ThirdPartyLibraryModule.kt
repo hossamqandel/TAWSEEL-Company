e96d071e05d1ddf7af506bcac8ca2ce8259491e0
Hossam <hossamegyqandel@gmail.com>
2022-11-30 17:35:16 +0200

    Provides glide and coroutine dispatcher in di

--- app/src/main/java/dev/hossam/tawseelcompany/di/ThirdPartyLibraryModule.kt
+++ app/src/main/java/dev/hossam/tawseelcompany/di/ThirdPartyLibraryModule.kt
@@ -1,10 +1,34 @@
 package dev.hossam.tawseelcompany.di
 
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.hossam.tawsel.core.DispatcherProvider
+import com.hossam.tawsel.core.StandardDispatchers
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ThirdPartyLibraryModule {
+
+    @Provides
+    @Singleton
+    fun provideDispatcherProvider(): DispatcherProvider {
+        return StandardDispatchers()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGlide(@ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+    )
+
 }
