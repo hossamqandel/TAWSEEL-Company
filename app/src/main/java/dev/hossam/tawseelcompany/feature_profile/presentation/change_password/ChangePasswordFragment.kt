6f9b5d5a5817b7564a528442f4ad743d0331242f
Hossam <hossamegyqandel@gmail.com>
2022-12-06 18:30:01 +0200

    init commit, added fragment_change_password, made navigation from profile to ChangePasswordFragment

---
+++ app/src/main/java/dev/hossam/tawseelcompany/feature_profile/presentation/change_password/ChangePasswordFragment.kt
@@ -0,0 +1,16 @@
+package dev.hossam.tawseelcompany.feature_profile.presentation.change_password
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dagger.hilt.android.AndroidEntryPoint
+import dev.hossam.tawseelcompany.R
+import dev.hossam.tawseelcompany.databinding.FragmentChangePasswordBinding
+import dev.hossam.tawseelcompany.feature_main.presentation.util.BaseFragment
+
+@AndroidEntryPoint
+class ChangePasswordFragment : BaseFragment<FragmentChangePasswordBinding>(FragmentChangePasswordBinding::inflate) {
+
+}
