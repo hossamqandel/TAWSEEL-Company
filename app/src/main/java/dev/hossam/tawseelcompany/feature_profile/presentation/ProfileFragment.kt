fd2cd2a026d2e8c5c387454e9a623dddf72bc5f6
Hossam <hossamegyqandel@gmail.com>
2022-12-06 16:11:07 +0200

    init commit, added fragment_profile

---
+++ app/src/main/java/dev/hossam/tawseelcompany/feature_profile/presentation/ProfileFragment.kt
@@ -0,0 +1,17 @@
+package dev.hossam.tawseelcompany.feature_profile.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dagger.hilt.android.AndroidEntryPoint
+import dev.hossam.tawseelcompany.R
+import dev.hossam.tawseelcompany.databinding.FragmentProfileBinding
+import dev.hossam.tawseelcompany.feature_main.presentation.util.BaseFragment
+
+@AndroidEntryPoint
+class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
+
+
+}
